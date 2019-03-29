80df8b9caf132ecd3b1120a5e0d38dcf6cb91fca
Charles Feval <chfeval@microsoft.com>
2019-03-29 11:38:35 -0700

    Moving solution to src

--- Readme.md
+++ Readme.md
@@ -20,6 +20,10 @@ The API is only returning Shared Access Signatures. In this
 demo it is not doing any validation. In real life it should
 validate that user has permissions to download/upload files.
 
+Note that to be allowed to upload directly to blob storage, 
+[CORS needs to be setup](https://docs.microsoft.com/en-us/rest/api/storageservices/cross-origin-resource-sharing--cors--support-for-the-azure-storage-services) 
+appropriately on the Storage Account.
+
 To download a file, the webpage is asking the API for a read-only SAS, then redirects the browser
 to this URL, which triggers the download directly from blob storage.
 
@@ -33,3 +37,37 @@ Have a look at:
 Note that this example is not implementing any access control. In real-life,
 the API would validate that the user has relevant permissions to upload / list / access the files
 they are requesting, before issuing SAS or file list.
+
+# To deploy the demo
+
+This demo contains a provisioning script that will automatically provision the necessary
+resources and deploy the application. For it to work you have two options:
+
+1. If you have the Azure CLI installed on your machine, then make sure you are currently
+    selecting the subscription on which you want the demo to be deployed, then simply run
+    `./provision.sh`.
+
+2. If you don't have the Azure CLI installed, go to the [Azure Shell](https://shell.azure.com),
+    select the subscription on which you want to provision the demo, then select "Bash" as
+    the shell type, and finally, run ``.
+
+# To cleanup the demo
+
+The provisioning script creates a cleanup script to delete all the resources it created.
+
+Run `./cleanup.sh` on the same place you ran the initial command (local CLI, or Azure Shell).
+
+Alternatively, simply delete the resource group that was created.
+
+# To run the demo locally
+
+This requires dotnet core 2.2 on the machine you are running.
+
+You need to provision a storage account and a container within it. If you have not ran the
+provisioning script, copy the appSettings template file into the `src` directory and rename
+it `appsettings.Development.json`, replace the `_APPSTORAGECONNECTIONSTRING_` and `_UPLOADCONTAINERNAME_`
+by respectively the connection string from the storage account you want to use, and the container name
+of the storage container you created.
+
+Go to the `src` folder and run `dotnet restore`, then `dotnet run`. The application should start
+on port 5000, so navigate to `http://localhost:5000` ; potentially ignore HTTPS warnings.
---
+++ provision.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+random() { size=$1; echo -n `date +%s%N | sha256sum | base64 | head -c $size`;}
+
+usage() {
+    echo "Usage: `basename "$0"` [--name $NAME] [--location $LOCATION]"
+    exit 1
+}
+
+PWD=`pwd`
+NAME=`basename "$PWD"`
+LOCATION="westus2"
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    shift
+
+    case $key in
+        -n|--name)
+            NAME="$1"
+            shift
+        ;;
+        -l|--location)
+            LOCATION="$1"
+            shift
+        ;;
+        *)
+            echo "Unknown parameter: $key"
+            usage
+        ;;
+    esac
+done
+
+
+RGNAME="$NAME"
+STORAGEBASENAME="`echo -n $NAME | head -c 15``random 5`"
+APPSTORAGENAME="`echo "$STORAGEBASENAME" | sed -e 's/-//g' | sed -E 's/^(.*)$/\L\1/g' | head -c 20`out"
+WEBAPPNAME="$NAME-`random 5`"
+UPLOADCONTAINERNAME="demo"
+DEPLOYMENTUSERNAME="$WEBAPPNAME"
+DEPLOYMENTPASSWORD="`random 30`"
+
+echo "Creating RG $RGNAME"
+az group create --name $RGNAME --location $LOCATION --query "properties.provisioningState" -o tsv
+
+echo "Generating cleanup script"
+echo "#!/bin/bash
+
+echo 'Removing resource group $RGNAME'
+az group delete --name $RGNAME --yes
+" > cleanup.sh
+chmod +x cleanup.sh
+
+echo "Creating storage account $APPSTORAGENAME"
+az storage account create --name $APPSTORAGENAME --kind StorageV2 --location $LOCATION -g $RGNAME --https-only true --query "provisioningState" -o tsv
+echo "Creating container $UPLOADCONTAINERNAME"
+az storage container create --name $UPLOADCONTAINERNAME --account-name $APPSTORAGENAME --query "created" -o tsv
+echo "Retrieving keys"
+APPSTORAGECONNECTIONSTRING=`az storage account show-connection-string -g $RGNAME -n $APPSTORAGENAME --query connectionString -o tsv`
+
+echo "Generating local settings file"
+ESCAPED_APPSTORAGECONNECTIONSTRING=`echo $APPSTORAGECONNECTIONSTRING | sed -e 's/\\//\\\\\\//g'`
+cat appsettings.Development.json.template |
+	sed "s/_APPSTORAGECONNECTIONSTRING_/$ESCAPED_APPSTORAGECONNECTIONSTRING/g" |
+	sed "s/_UPLOADCONTAINERNAME_/$UPLOADCONTAINERNAME/g" |
+	> src/appsettings.Development.json
+
+echo "Creating app service plan $WEBAPPNAME"
+az webapp deployment user set --user-name "$DEPLOYMENTUSERNAME" --password "$DEPLOYMENTPASSWORD" > /dev/null
+az appservice plan create -g $RGNAME -n $WEBAPPNAME --sku FREE --location $LOCATION --query "provisioningState" -o tsv
+echo "Creating webapp $WEBAPPNAME"
+HOSTNAME=`az webapp create -g $RGNAME -n $WEBAPPNAME --plan $WEBAPPNAME --deployment-local-git --query "defaultHostName" -o tsv`
+az webapp config appsettings set --name $WEBAPPNAME -g $RGNAME --settings Storage.ConnectionString="$APPSTORAGECONNECTIONSTRING" Storage.ContainerName="$UPLOADCONTAINERNAME" > /dev/null
+echo "Deploying webapp"
+cd src
+dotnet clean
+dotnet restore
+dotnet build
+cd 
+git init
+git remote add azure "https://$DEPLOYMENTUSERNAME:$DEPLOYMENTPASSWORD@$WEBAPPNAME.scm.azurewebsites.net/$WEBAPPNAME.git"
+git add -A
+git commit 
+cd $PWD
+
+echo "           Web app name: $WEBAPPNAME"
+echo "            Web app url: $HOSTNAME"
+echo "        Deployment user: $DEPLOYMENTUSERNAME"
+echo "    Deployment password: $DEPLOYMENTPASSWORD"
+echo "        Storage account: $APPSTORAGENAME"
+echo "            Storage key: $APPSTORAGECONNECTIONSTRING"
+echo "    Resource group name: $RGNAME"
+echo "         Container name: $UPLOADCONTAINERNAME"
